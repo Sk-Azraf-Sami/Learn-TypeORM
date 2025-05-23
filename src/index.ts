fd3b4182f7ff840dd2ce471fa2f86e548035172f
Sk-Azraf-Sami <azraf7026official@gmail.com>
2025-05-23 21:13:49 +0600

    entities

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import  {DataSource} from 'typeorm'
+import { Client } from './entities/Client'
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost", 
+    port: 5432, 
+    username: "postgres", 
+    password: "1234",
+    database: "learntypeorm", 
+    /*
+    entities: []
+    This is where you list your 
+    TypeORM entity classes (models). 
+    An empty array means no entities 
+    are registered, so no tables will 
+    be created or managed.
+
+    synchronize: true
+    TypeORM will automatically create or 
+    update the database tables to match 
+    your entity classes every time the app starts.
+    */
+    entities: [Client], 
+    synchronize: true
+})
+
+AppDataSource.initialize()
+    .then(()=>{
+        console.log("Data Source has been initialized")
+    })
+    .catch((err)=>{
+        console.error("Error during data source initialized", err)
+    })
+
+console.log("test!");
